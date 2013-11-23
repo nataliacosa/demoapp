37d8d225a9227f34891da2c2433544ab89f167ae
Natalia Cosa <natalia.cosa@gmail.com>
2013-11-23 19:38:39 +0000

    crashing it

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,18 +1,11 @@
 require 'spec_helper'
- 
-describe "Static pages" do
-  describe "Home page" do
-    it "should have the content 'Demo App'" do
-      visit root_path
-      expect(page).to have_content('Demo App')
+
+describe "StaticPages" do
+  describe "GET /static_pages" do
+    it "works! (now write some real specs)" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      get static_pages_index_path
+      response.status.should be(200)
     end
   end
- 
- describe "Help page" do
-    it "should have the content 'Help'" do
-      visit help_path
-      expect(page).to have_content('Help')
-    end
-  end   
 end
-  
